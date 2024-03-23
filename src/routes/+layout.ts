5c3b69a3515695f25d0d8f2f3148d15bea0db720
naturesquad <lucy.marie.neal@gmail.com>
2024-03-23 11:44:01 -0700

    #1: Setup deploy pipeline

---
+++ src/routes/+layout.ts
@@ -0,0 +1,8 @@
+import { redirect } from "@sveltejs/kit";
+import { APP_REDIRECTS } from "../redirect";
+export async function load({ url }) {
+	const pathname = url.pathname;
+	if (APP_REDIRECTS.hasOwnProperty(pathname)) {
+		return redirect(301, (APP_REDIRECTS as any)[pathname]);
+	}
+}
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { APP_REDIRECTS } from './src/redirect.js';
 
 const dev = process.argv.includes('dev');
 
@@ -12,6 +13,9 @@ const config = {
 		}),
 		paths: {
 			base: dev ? '' : process.env.BASE_PATH,
+		},
+		prerender: {
+			entries: ['*', ...Object.keys(APP_REDIRECTS)],
 		}
 	},
 	preprocess: vitePreprocess()
